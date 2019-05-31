358d6dc7b4d5af639d262847db99ab594b45b212
Glitch (amdtel-blockchain-dev-pwa-3box-dapp) <none>
2019-05-31 18:35:33 +0000

    🕋🍬 Checkpoint ./src/components/my-view1.js:422972/412

--- src/components/my-view1.js
+++ src/components/my-view1.js
@@ -45,12 +45,12 @@ class MyView1 extends PageViewElement {
   firstUpdated() {
     if (typeof window.ethereum !== 'undefined' || typeof window.web3 !== 'undefined') {
       //Use Mist/MetaMask's provider
-      this.web3js = new window.Web3(window.web3.currentProvider);
-      this.account = this.web3js.currentProvider.selectedAddress;
+      this.ethProvider = window['ethereum'] || window.web3.currentProvider;
+      if (this.ethProvider.selectedAddress!=='undefined') {
+        this
+      };
     } else {
       console.log('No web3! You will need to install MetaMask!')
-      // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-      this.web3js = new window.Web3(new window.Web3.providers.HttpProvider("http://localhost:8545"));
     }
     // this.loadBlockchainData();
   }
