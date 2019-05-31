c59dbd76a537823a4e022475e42320ded2da1825
Glitch (amdtel-blockchain-dev-pwa-3box-dapp) <none>
2019-05-31 12:49:14 +0000

    🍫🕴 Checkpoint ./src/components/pwa-dapp.js:422972/43 ./package.json:422972/1143

--- src/components/pwa-dapp.js
+++ src/components/pwa-dapp.js
@@ -14,6 +14,7 @@ import { installMediaQueryWatcher } from 'pwa-helpers/media-query.js';
 import { installOfflineWatcher } from 'pwa-helpers/network.js';
 import { installRouter } from 'pwa-helpers/router.js';
 import { updateMetadata } from 'pwa-helpers/metadata.js';
+import '3box';
 
 // This element is connected to the Redux store.
 import { store } from '../store.js';
